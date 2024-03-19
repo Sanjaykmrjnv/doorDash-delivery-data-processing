00eb4b4aa562f0f1bb32d33fc735e0a14f400d18
Sanjay Kumar <sanjaykmrjnv@gmail.com>
2024-03-19 10:59:36 +0530

    modified:   doordash_lambda_function.py

--- doordash_lambda_function.py
+++ doordash_lambda_function.py
@@ -1,29 +1,49 @@
+import json
 import boto3
-import pandas  as pd
+import pandas as pd
 
 def lambda_handler(event, context):
-    print("Event -> ",event)
-    print("Context -> ",context)
-
-    s3_bucket = event['Records'][0]['s3']['bucket']['name']
-    s3_key = event['Records'][0]['s3']['object']['key']
-
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Initialize S3 client
     s3 = boto3.client('s3')
-    obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)
-    df = pd.read_json(obj['Body'])
-
-    delivered_records = df[df['status'] == 'delivered']
-
-    target_bucket = 'doordash-target-sa'
-    target_key = s3_key.replace('landing', 'target')  # Adjust key to target bucket
-    target_obj = {'Bucket': target_bucket, 'Key': target_key}
-    s3.put_object(Body=delivered_records.to_json(), **target_obj)
-
-    sns = boto3.client('sns')
-    topic_arn = 'arn:aws:sns:us-east-1:767397935988:doordash-notification-service'  # Specify your SNS topic ARN
-    sns.publish(TopicArn=topic_arn, Message='Delivery data processing completed successfully.')
-
-    return {
-        'statusCode': 200,
-        'body': 'Processing completed.'
-    }
+    
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        data = response['Body'].read().decode('utf-8')
+        json_data = json.loads(data)
+        
+        df = pd.DataFrame(json_data)
+        
+        df_delivered = df[df['status'] == 'delivered']
+        
+        json_filtered = df_delivered.to_json(orient='records')
+        
+        target_bucket = 'doordash-target-sa'
+        target_key = f"{key.split('/')[-1].split('.')[0]}-filtered.json"  # Change the filename as needed
+        s3.put_object(Bucket=target_bucket, Key=target_key, Body=json_filtered)
+        
+        # Publish a success message to SNS topic
+        sns = boto3.client('sns')
+        sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:767397935988:doordash-notification-service',
+            Message='Data processing completed successfully.'
+        )
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Data processing completed successfully.')
+        }
+    except Exception as e:
+        # Publish a failure message to SNS topic
+        sns = boto3.client('sns')
+        sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:767397935988:doordash-notification-service',
+            Message=f'Data processing failed: {str(e)}'
+        )
+        
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Data processing failed: {str(e)}')
+        }
